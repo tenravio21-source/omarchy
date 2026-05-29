3d4acafcf29339e6ffc3499f72ad019b4a815570
rio ten <ravioten05@gmail.com>
2026-05-29 22:44:00 +0530

    kitty

--- NvChad/lua/configs/lspconfig.lua
+++ NvChad/lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "lua-ls", "dartls", "rust_analyzer", "pyrefly", "gopls", "vtsls", "eslint", "html-lsp", "jdtls" }
+local servers =
+	{ "lua-ls", "dartls", "rust_analyzer", "pyrefly", "gopls", "vtsls", "eslint", "html-lsp", "jdtls", "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
