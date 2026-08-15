837f56d1f9deb83f16b33ea64baf53fdfe53308e
rio ten <ravioten05@gmail.com>
2026-08-15 21:32:09 +0530

    nv

---
+++ yazi/init.lua
@@ -0,0 +1,10 @@
+require("real-icons"):setup({
+	pack = "flow-deep",
+	packs = {
+		["flow-deep"] = {
+			type = "vscode",
+			path = "~/.vscode/extensions/thang-nm.flow-icons-2.0.9",
+			theme = "flow-deep",
+		},
+	},
+})
